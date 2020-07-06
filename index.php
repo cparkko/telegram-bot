a09a903485fb88dfba1c2a706fc09c3a0210e281
k.park <k.park@creagames.ru>
2020-07-06 16:09:16 +0300

    Fix 4

--- index.php
+++ index.php
@@ -128,7 +128,7 @@
 	        'resize_keyboard' => true)));
 	    } else if ($text === "Hello" || $text === "Hi") {
 	      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Nice to meet you'));
-	    } else if (strpos($text, "Десятина")) {
+	    } else if (stripos($text, "Десятина") === 0 || stripos($text, "Десятина") > 0) {
 	    	apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Ланистеры всегда платят долги'));
 	    } else if (strpos($text, "/stop") === 0) {
 	      // stop now
