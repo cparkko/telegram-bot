101dc18457c7f578ff550ff68515c24df5c85de1
k.park <k.park@creagames.ru>
2020-07-13 17:51:09 +0300

    added buttons inline 2

--- index.php
+++ index.php
@@ -128,13 +128,13 @@
 	        'resize_keyboard' => true)));
 	    } else if ($text === "Hello" || $text === "Hi") {
 	      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Nice to meet you'));
-	    } else if (strpos($text, "test") === 0) {
-	    	apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Test OK!', 'inline_markup' => [
+	    } else if (stripos($text, "test") === 0) {
+	    	apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Test OK!',
 	    		'inline_keyboard' => [
 	    			['text' => 'Google', 'url' => 'https://google.ru', 'callback_data' => 'Google pressed'],
 	    			['text' => 'Yandex', 'url' => 'https://yandex.ru', 'callback_data' => 'Yandex pressed'],
 	    		]
-	    	]));
+	    	));
 	    } else if (strpos($text, "/stop") === 0) {
 	      // stop now
 	    } else {
